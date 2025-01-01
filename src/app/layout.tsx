6d79a4216711b7f362f8c0cabcab0e9408d9471c
maxlibin <me@maxlibin.com>
2025-01-01 22:15:14 +0800

    Add copilotkit runtime

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { CopilotKit } from "@copilotkit/react-core";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className}>
-      <body>{children}</body>
+      <body>
+        <CopilotKit runtimeUrl="/api/copilot">{children}</CopilotKit>
+      </body>
     </html>
   );
 }
